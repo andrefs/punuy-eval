5ef01f0f22877e5c67d431e03e0b569047acc358
André Santos <andrefs@andrefs.com>
2023-12-05 11:48:18 +0000

    save compare-mc30 to file

--- src/lib/experiments/compare-mc30.ts
+++ src/lib/experiments/compare-mc30.ts
@@ -1,36 +1,38 @@
-import mc30 from "punuy-datasets/mc30"
-import rg65 from "punuy-datasets/rg65"
-import ws353 from "punuy-datasets/ws353"
-import pcorrtest from "@stdlib/stats-pcorrtest"
+import mc30 from "punuy-datasets/mc30";
+import rg65 from "punuy-datasets/rg65";
+import ws353 from "punuy-datasets/ws353";
+import pcorrtest from "@stdlib/stats-pcorrtest";
+import fs from "fs/promises";
+import oldFs from "fs";
 
-import { Model, ModelIds, gpt35turbo, gpt4, gpt4turbo } from "../models"
-import { JsonSyntaxError } from "../validation"
-import logger from "../logger"
+import { Model, ModelIds, gpt35turbo, gpt4, gpt4turbo } from "../models";
+import { JsonSyntaxError } from "../validation";
+import logger from "../logger";
 
 interface DatasetScores {
   [word1: string]: {
     [word2: string]: {
-      [dataset: string]: number
-    }
-  }
+      [dataset: string]: number;
+    };
+  };
 }
 
 type ModelsResults = {
-  [key in ModelIds]: string[]
-}
+  [key in ModelIds]: string[];
+};
 
 export const loadDatasetScores = async () => {
-  const pairs: DatasetScores = {}
+  const pairs: DatasetScores = {};
 
   for (const part of mc30.partitions) {
     for (const entry of part.data) {
-      pairs[entry.word1] = pairs[entry.word1] || {}
-      pairs[entry.word1][entry.word2] = pairs[entry.word1][entry.word2] || {}
+      pairs[entry.word1] = pairs[entry.word1] || {};
+      pairs[entry.word1][entry.word2] = pairs[entry.word1][entry.word2] || {};
       if ("value" in entry) {
-        pairs[entry.word1][entry.word2]["mc30"] = entry.value
+        pairs[entry.word1][entry.word2]["mc30"] = entry.value;
       } else {
         pairs[entry.word1][entry.word2]["mc30"] =
-          entry.values.reduce((a, b) => a + b, 0) / entry.values.length
+          entry.values.reduce((a, b) => a + b, 0) / entry.values.length;
       }
     }
   }
@@ -40,12 +42,12 @@ export const loadDatasetScores = async () => {
       const value =
         "value" in entry
           ? entry.value
-          : entry.values.reduce((a, b) => a + b, 0) / entry.values.length
+          : entry.values.reduce((a, b) => a + b, 0) / entry.values.length;
 
       if (entry.word1 in pairs && entry.word2 in pairs[entry.word1]) {
-        pairs[entry.word1][entry.word2][rg65.id] = value
+        pairs[entry.word1][entry.word2][rg65.id] = value;
       } else if (entry.word2 in pairs && entry.word1 in pairs[entry.word2]) {
-        pairs[entry.word2][entry.word1][rg65.id] = value
+        pairs[entry.word2][entry.word1][rg65.id] = value;
       }
     }
   }
@@ -55,41 +57,41 @@ export const loadDatasetScores = async () => {
       const value =
         "value" in entry
           ? entry.value
-          : entry.values.reduce((a, b) => a + b, 0) / entry.values.length
+          : entry.values.reduce((a, b) => a + b, 0) / entry.values.length;
 
       if (entry.word1 in pairs && entry.word2 in pairs[entry.word1]) {
-        pairs[entry.word1][entry.word2][ws353.id] = value * (4 / 10)
+        pairs[entry.word1][entry.word2][ws353.id] = value * (4 / 10);
       } else if (entry.word2 in pairs && entry.word1 in pairs[entry.word2]) {
-        pairs[entry.word2][entry.word1][ws353.id] = value * (4 / 10)
+        pairs[entry.word2][entry.word1][ws353.id] = value * (4 / 10);
       }
     }
   }
 
   // chord vs cord
   // see http://dx.doi.org/10.1162/coli.2006.32.1.13
-  delete pairs["chord"]
+  delete pairs["chord"];
 
-  return pairs
-}
+  return pairs;
+};
 
 const getPairs = (scores: DatasetScores) => {
-  const pairs: [string, string][] = []
+  const pairs: [string, string][] = [];
 
   for (const word1 in scores) {
     for (const word2 in scores[word1]) {
-      pairs.push([word1, word2])
+      pairs.push([word1, word2]);
     }
   }
 
-  return pairs
-}
+  return pairs;
+};
 
-const name = "compare-mc30"
+const name = "compare-mc30";
 const description =
-  "Compare the scores of multiple AI models with the scores from multiple human annotations of the MC30 pair set."
+  "Compare the scores of multiple AI models with the scores from multiple human annotations of the MC30 pair set.";
 const genPrompt = (pairs: string[][]) =>
   'Please rate the similarity of the following pairs of words on a scale of 0 to 4, where 0 means "completely unrelated" and 4 means "very similar". Fractional values are allowed.\n\n' +
-  pairs.map(([w1, w2]) => `${w1} ${w2}`).join("\n")
+  pairs.map(([w1, w2]) => `${w1} ${w2}`).join("\n");
 
 // TODO fix when OpenAI supports items with tuple definition in JSON schema
 // https://community.openai.com/t/json-schema-tuple-validation-support/273554
@@ -107,51 +109,53 @@ const resultSchema = {
       },
     },
   },
-}
+};
 
 async function runTrialModel(model: Model, prompt: string) {
   const f = {
     name: "evaluate_scores",
     description: "Evaluate the word similarity scores.",
     parameters: resultSchema,
-  }
+  };
 
-  const res = await model.makeRequest(prompt, { function: f })
-  return res
+  const res = await model.makeRequest(prompt, { function: f });
+  return res;
 }
 
 async function runTrialsModel(trials: number, model: Model, prompt: string) {
-  logger.info(`  model ${model.modelId}.`)
-  logger.debug(`Prompt: ${prompt}`)
-  const results = []
+  logger.info(`  model ${model.modelId}.`);
+  logger.debug(`Prompt: ${prompt}`);
+  const results = [];
   for (let i = 0; i < trials; i++) {
-    logger.info(`    trial #${i + 1} of ${trials}`)
-    const res = await runTrialModel(model, prompt)
+    logger.info(`    trial #${i + 1} of ${trials}`);
+    const res = await runTrialModel(model, prompt);
     results.push(
       res.type === "openai"
         ? res.data.choices[0].message.tool_calls?.[0].function.arguments || ""
-        : "",
-    )
+        : ""
+    );
   }
-  return results
+  return results;
 }
 
 async function runTrials(trials: number) {
-  const scores = await loadDatasetScores()
-  const pairs = getPairs(scores)
-  const prompt = genPrompt(pairs)
+  const scores = await loadDatasetScores();
+  const pairs = getPairs(scores);
+  const prompt = genPrompt(pairs);
 
-  logger.info(`Running experiment ${name} ${trials} times.`)
+  logger.info(
+    `Running experiment ${name} with ${trials} trials on models [gpt35turbo, gpt4, gpt4turbo].`
+  );
 
-  const gpt35turbo_res = await runTrialsModel(trials, gpt35turbo, prompt)
-  const gpt4_res = await runTrialsModel(trials, gpt4, prompt)
-  const gpt4turbo_res = await runTrialsModel(trials, gpt4turbo, prompt)
+  const gpt35turbo_res = await runTrialsModel(trials, gpt35turbo, prompt);
+  const gpt4_res = await runTrialsModel(trials, gpt4, prompt);
+  const gpt4turbo_res = await runTrialsModel(trials, gpt4turbo, prompt);
 
   return {
     gpt35turbo: gpt35turbo_res,
     gpt4: gpt4_res,
     gpt4turbo: gpt4turbo_res,
-  }
+  };
 }
 
 interface MC30Results {
@@ -159,17 +163,17 @@ interface MC30Results {
     [word2: string]: {
       models: {
         [model: string]: {
-          avg: number
-          values: number[]
-          sum: number
-          count: number
-        }
-      }
+          avg: number;
+          values: number[];
+          sum: number;
+          count: number;
+        };
+      };
       human: {
-        [dataset: string]: number
-      }
-    }
-  }
+        [dataset: string]: number;
+      };
+    };
+  };
 }
 
 function unzipResults(results: MC30Results) {
@@ -180,140 +184,193 @@ function unzipResults(results: MC30Results) {
     mc30: [] as number[],
     rg65: [] as number[],
     ws353: [] as number[],
-  }
+  };
 
   for (const w1 in results) {
     for (const w2 in results[w1]) {
-      res.gpt35turbo.push(results[w1][w2].models.gpt35turbo.avg)
-      res.gpt4.push(results[w1][w2].models.gpt4.avg)
-      res.gpt4turbo.push(results[w1][w2].models.gpt4turbo.avg)
-      res.mc30.push(results[w1][w2].human.mc30)
-      res.rg65.push(results[w1][w2].human.rg65)
-      res.ws353.push(results[w1][w2].human.ws353)
+      res.gpt35turbo.push(results[w1][w2].models.gpt35turbo.avg);
+      res.gpt4.push(results[w1][w2].models.gpt4.avg);
+      res.gpt4turbo.push(results[w1][w2].models.gpt4turbo.avg);
+      res.mc30.push(results[w1][w2].human.mc30);
+      res.rg65.push(results[w1][w2].human.rg65);
+      res.ws353.push(results[w1][w2].human.ws353);
     }
   }
 
-  return res
+  return res;
 }
 
-async function validate(modelsRes: ModelsResults, humanScores: DatasetScores) {
+async function validate(
+  modelsRes: ModelsResults,
+  humanScores: DatasetScores,
+  trials: number
+) {
   try {
-    const res = mergeResults(modelsRes, humanScores)
-    const arrays = unzipResults(res)
-    const corrMat = calcCorrelation(Object.values(arrays))
+    const res = mergeResults(modelsRes, humanScores);
+    const arrays = unzipResults(res);
+    const corrMat = calcCorrelation(Object.values(arrays));
 
-    printMatrix(Object.keys(arrays), corrMat)
+    printMatrix(Object.keys(arrays), corrMat);
+    const varNames = Object.keys(arrays);
+
+    const tests = {} as { [dsVsds: string]: string };
+    for (let i = 0; i < varNames.length - 1; i++) {
+      for (let j = i + 1; j < varNames.length; j++) {
+        const r = corrMat[i][j];
+        tests[`${varNames[i]} vs ${varNames[j]}`] = r.print();
+      }
+    }
+
+    const traceId = Date.now();
+    const log: MC30LogFile = {
+      trials,
+      traceId,
+      results: res,
+      arrays,
+      corrMat,
+      varNames: Object.keys(arrays),
+      tests,
+    };
+
+    await saveFile(log);
   } catch (e) {
-    return new JsonSyntaxError()
+    return new JsonSyntaxError();
   }
+}
+
+async function saveFile(log: MC30LogFile) {
+  const traceId = log.traceId;
+  const rootFolder = "./results";
+  const filename = `${rootFolder}/${traceId}_${name}.json`;
+  logger.info(
+    `Saving experiment ${name} with ${log.trials} trials to ${filename}.`
+  );
+
+  const json = JSON.stringify(log, null, 2);
+
+  if (!oldFs.existsSync(rootFolder)) {
+    await fs.mkdir(rootFolder);
+  }
+
+  await fs.writeFile(filename, json);
+}
+
+interface MC30LogFile {
+  trials: number;
+  traceId: number;
+  results: MC30Results;
+  arrays: ReturnType<typeof unzipResults>;
+  corrMat: ReturnType<typeof calcCorrelation>;
+  tests: { [dsVsds: string]: string };
+  varNames: string[];
 }
 
 function printMatrix(
   varNames: string[],
-  matrix: ReturnType<typeof pcorrtest>[][],
+  matrix: ReturnType<typeof pcorrtest>[][]
 ) {
   for (let i = 0; i < varNames.length - 1; i++) {
     for (let j = i + 1; j < varNames.length; j++) {
-      const r = matrix[i][j]
-      console.log(`----------------------\n${varNames[i]} vs ${varNames[j]}`)
-      console.log(r.print())
+      const r = matrix[i][j];
+      console.log(`----------------------\n${varNames[i]} vs ${varNames[j]}`);
+      console.log(r.print());
     }
   }
 
-  console.log("\n\n," + varNames.join(","))
+  console.log("\n\n," + varNames.join(","));
   for (let i = 0; i < varNames.length; i++) {
     console.log(
-      [varNames[i], ...matrix[i].map(r => r?.pcorr.toFixed(2))].join(","),
-    )
+      [varNames[i], ...matrix[i].map(r => r?.pcorr.toFixed(2))].join(",")
+    );
   }
-  console.log("\n\n")
+  console.log("\n\n");
 }
 
 function calcCorrelation(data: number[][]) {
-  const corrMatrix = [] as ReturnType<typeof pcorrtest>[][]
+  const corrMatrix = [] as ReturnType<typeof pcorrtest>[][];
 
   for (let i = 0; i < data.length; i++) {
-    corrMatrix[i] = []
+    corrMatrix[i] = [];
     for (let j = 0; j < data.length; j++) {
       if (i < j) {
-        const corr = pcorrtest(data[i], data[j])
-        corrMatrix[i][j] = corr
+        const corr = pcorrtest(data[i], data[j]);
+        corrMatrix[i][j] = corr;
       }
     }
   }
-  return corrMatrix
+  return corrMatrix;
 }
 
 function mergeResults(modelsRes: ModelsResults, humanScores: DatasetScores) {
-  const res = {} as MC30Results
+  const res = {} as MC30Results;
 
   try {
-    const gpt35turbo = modelsRes.gpt35turbo.map(r => JSON.parse(r)) // TODO cast to resultSchema somehow
-    const gpt4 = modelsRes.gpt4.map(r => JSON.parse(r))
-    const gpt4turbo = modelsRes.gpt4turbo.map(r => JSON.parse(r))
+    const gpt35turbo = modelsRes.gpt35turbo.map(r => JSON.parse(r)); // TODO cast to resultSchema somehow
+    const gpt4 = modelsRes.gpt4.map(r => JSON.parse(r));
+    const gpt4turbo = modelsRes.gpt4turbo.map(r => JSON.parse(r));
 
-    let modelName = "gpt35turbo"
+    let modelName = "gpt35turbo";
     for (const score of gpt35turbo.flatMap(({ scores }) => [...scores])) {
-      const [w1, w2] = score.words
-      res[w1] = res[w1] || {}
-      res[w1][w2] = res[w1][w2] || { human: {}, models: {} }
+      const [w1, w2] = score.words;
+      res[w1] = res[w1] || {};
+      res[w1][w2] = res[w1][w2] || { human: {}, models: {} };
       res[w1][w2].models[modelName] = res[w1][w2].models[modelName] || {
         values: [],
-      }
-      res[w1][w2].models[modelName].values.push(score.score)
+      };
+      res[w1][w2].models[modelName].values.push(score.score);
     }
     for (const w1 in res) {
       for (const w2 in res[w1]) {
         res[w1][w2].models[modelName].avg =
           res[w1][w2].models[modelName].values.reduce((a, b) => a + b, 0) /
-          res[w1][w2].models[modelName].values.length
+          res[w1][w2].models[modelName].values.length;
       }
     }
 
-    modelName = "gpt4"
+    modelName = "gpt4";
     for (const score of gpt4.flatMap(({ scores }) => [...scores])) {
-      const [w1, w2] = score.words
-      res[w1] = res[w1] || {}
-      res[w1][w2] = res[w1][w2] || { human: {}, models: {} }
+      const [w1, w2] = score.words;
+      res[w1] = res[w1] || {};
+      res[w1][w2] = res[w1][w2] || { human: {}, models: {} };
       res[w1][w2].models[modelName] = res[w1][w2].models[modelName] || {
         values: [],
-      }
-      res[w1][w2].models[modelName].values.push(score.score)
+      };
+      res[w1][w2].models[modelName].values.push(score.score);
     }
     for (const w1 in res) {
       for (const w2 in res[w1]) {
         res[w1][w2].models[modelName].avg =
           res[w1][w2].models[modelName].values.reduce((a, b) => a + b, 0) /
-          res[w1][w2].models[modelName].values.length
+          res[w1][w2].models[modelName].values.length;
       }
     }
 
-    modelName = "gpt4turbo"
+    modelName = "gpt4turbo";
     for (const score of gpt4turbo.flatMap(({ scores }) => [...scores])) {
-      const [w1, w2] = score.words
-      res[w1] = res[w1] || {}
-      res[w1][w2] = res[w1][w2] || { human: {}, models: {} }
+      const [w1, w2] = score.words;
+      res[w1] = res[w1] || {};
+      res[w1][w2] = res[w1][w2] || { human: {}, models: {} };
       res[w1][w2].models[modelName] = res[w1][w2].models[modelName] || {
         values: [],
-      }
-      res[w1][w2].models[modelName].values.push(score.score)
+      };
+      res[w1][w2].models[modelName].values.push(score.score);
     }
     for (const w1 in res) {
       for (const w2 in res[w1]) {
         res[w1][w2].models[modelName].avg =
           res[w1][w2].models[modelName].values.reduce((a, b) => a + b, 0) /
-          res[w1][w2].models[modelName].values.length
+          res[w1][w2].models[modelName].values.length;
       }
     }
 
     for (const w1 in humanScores) {
       for (const w2 in humanScores[w1]) {
-        res[w1][w2].human = humanScores[w1][w2]
+        res[w1][w2].human = humanScores[w1][w2];
       }
     }
-    return res
+    return res;
   } catch (e) {
-    throw new JsonSyntaxError()
+    throw new JsonSyntaxError();
   }
 }
 
@@ -324,6 +381,6 @@ const CompareMC30Experiment = {
   schema: resultSchema,
   runTrials,
   validate,
-}
+};
 
-export default CompareMC30Experiment
+export default CompareMC30Experiment;
--- src/scripts/compareMC30.ts
+++ src/scripts/compareMC30.ts
@@ -1,19 +1,19 @@
-import { compareMc30 } from "../lib/experiments"
-import { loadDatasetScores } from "../lib/experiments/compare-mc30"
-import logger from "../lib/logger"
+import { compareMc30 } from "../lib/experiments";
+import { loadDatasetScores } from "../lib/experiments/compare-mc30";
+import logger from "../lib/logger";
 
-const trials = process.argv[2] ? parseInt(process.argv[2]) : 1
+const trials = process.argv[2] ? parseInt(process.argv[2]) : 1;
 
 const compareMC30 = async () => {
-  logger.info("Starting")
-  const res = await compareMc30.runTrials(trials)
+  logger.info("Starting");
+  const res = await compareMc30.runTrials(trials);
 
-  const humanScores = await loadDatasetScores()
+  const humanScores = await loadDatasetScores();
 
-  await compareMc30.validate(res, humanScores)
-}
+  await compareMc30.validate(res, humanScores, trials);
+};
 
 compareMC30().then(() => {
-  logger.info("Done")
-  process.exit(0)
-})
+  logger.info("Done");
+  process.exit(0);
+});
