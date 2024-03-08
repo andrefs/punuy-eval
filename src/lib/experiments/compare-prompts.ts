f9420c9d1cecb81786969d196387e31fc7154117
André Santos <andrefs@andrefs.com>
2024-03-08 23:25:27 +0000

    compare prompts

--- src/lib/experiments/compare-prompts.ts
+++ src/lib/experiments/compare-prompts.ts
@@ -1,37 +1,41 @@
-import { Model, gpt4, gpt4turbo, gpt35turbo } from "../models";
+import fs from "fs/promises";
+import oldFs from "fs";
 import logger from "../logger";
 import {
-  DatasetScores,
-  loadDatasetScores,
-} from "../dataset-adapters/collection";
-import { Prompt, TrialsResult } from ".";
+  ExpVarMatrix,
+  ExpVars,
+  ExpVarsFixedPrompt,
+  ExperimentData,
+  Prompt,
+  TrialsResult,
+  genValueCombinations,
+  getVarIds,
+} from ".";
 const name = "compare-prompts";
 const description = "Compare the results obtained with different prompts";
 
-const prompts: Prompts = {
-  simplest: {
-    type: "relatedness",
+const prompts: Prompt[] = [
+  {
+    id: "simplest",
+    types: ["relatedness"],
     text: "Indicate how strongly the words in each pair are related in meaning using integers from 1 to 5, where 1 means very unrelated and 5 means very related.",
   },
-  simpleScale: {
-    type: "relatedness",
+  {
+    id: "simpleScale",
+    types: ["relatedness"],
     text: "Indicate how strongly the words in each pair are related in meaning using integers from 1 to 5, where the scale means: 1 - not at all related, 2 - vaguely related, 3 - indirectly related, 4 - strongly related, 5 - inseparably related.",
   },
-  adaptedWs353: {
-    type: "relatedness",
+  {
+    id: "adaptedWs353",
+    types: ["relatedness"],
     text: 'Hello, we kindly ask you to assist us in a psycholinguistic experiment, aimed at estimating the semantic relatedness of various words in the English language. The purpose of this experiment is to assign semantic relatedness scores to pairs of words, so that machine learning algorithms can be subsequently trained and adjusted using human-assigned scores. Below is a list of pairs of words. For each pair, please assign a numerical relatedness score between 1 and 5 (1 = words are totally unrelated, 5 = words are VERY closely related). By definition, the relatedness of the word to itself should be 5. You may assign fractional scores (for example, 3.5).  When estimating relatedness of antonyms, consider them "related" (i.e., belonging to the same domain or representing features of the same concept), rather than "unrelated". Thank you for your assistance!',
   },
-  simlex999: {
-    type: "similarity",
+  {
+    id: "simlex999",
+    types: ["similarity"],
     text: "Two words are synonyms if they have very similar meanings. Synonyms represent the same type or category of thing. Here are some examples of synonym pairs: cup/mug, glasses/spectacles, envy/jealousy. In practice, word pairs that are not exactly synonymous may still be very similar. Here are some very similar pairs - we could say they are nearly synonyms: alligator/crocodile, love / affection, frog/toad. In contrast, although the following word pairs are related, they are not very similar. The words represent entirely different types of thing:car/tyre, car/motorway, car/crash, In this survey, you are asked to compare word pairs and to rate how similar they are by moving a slider. Remember, things that are related are not necessarily similar. If you are ever unsure, think back to the examples of synonymous pairs (glasses/spectacles), and consider how close the words are (or are not) to being synonymous. There is no right answer to these questions. It is perfectly reasonable to use your intuition or gut feeling as a native English speaker, especially when you are asked to rate word pairs that you think are not similar at all.",
   },
-};
-
-const models = {
-  gpt35turbo,
-  gpt4,
-  gpt4turbo,
-};
+];
 
 const resultSchema = {
   type: "object",
@@ -49,84 +53,107 @@ const resultSchema = {
   },
 };
 
-async function runTrialModel(model: Model, dsId: string, promptId: string) {
+async function runTrial(vars: ExpVarsFixedPrompt) {
   const f = {
     name: "evaluate_scores",
     description: "Evaluate the word similarity or relatedness scores",
     parameters: resultSchema,
   };
-  const res = await model.makeRequest(prompts[promptId].text, { function: f });
+  const res = await vars.model.makeRequest(vars.prompt.text, { function: f });
   return res;
 }
 
-async function runTrialsModel(
-  trials: number,
-  model: Model,
-  dsId: string,
-  promptId: string
-) {
-  const results = [];
-  logger.info(`  model ${model.id}.`);
-  logger.debug(`Prompt ID: ${promptId}`);
+async function runTrials(vars: ExpVars, trials: number) {
+  const prompt =
+    "generate" in vars.prompt ? vars.prompt.generate(vars) : vars.prompt;
+  logger.info(
+    `Running experiment ${name} ${trials} times on model ${vars.model.id}.`
+  );
+  logger.debug(`Prompt (${prompt.id}): ${prompt.text}`);
 
+  const results: string[] = [];
   for (let i = 0; i < trials; i++) {
     logger.info(`    trial #${i + 1} of ${trials}`);
-    const res = await runTrialModel(model, dsId, prompts[promptId].text);
+    const res = await runTrial({ ...vars, prompt });
     results.push(
       res.type === "openai"
         ? res.data.choices[0].message.tool_calls?.[0].function.arguments || ""
         : ""
     );
   }
-  return results;
+  return {
+    variables: vars,
+    data: results,
+  };
 }
 
-async function runTrials(trials: number): Promise<TrialsResult[]> {
-  const datasetIds = ["ws353", "simlex999"];
-  const datasets: { [key: string]: DatasetScores } = {};
-  for (const dsId of datasetIds) {
-    datasets[dsId] = await loadDatasetScores(dsId);
-  }
+async function perform(vars: ExpVars, trials: number, traceId?: number) {
+  const trialsRes = await runTrials(vars, trials);
 
+  const expData: ExperimentData = {
+    meta: {
+      name,
+      traceId: traceId ?? Date.now(),
+      schema: resultSchema,
+    },
+    variables: vars,
+    results: {
+      raw: trialsRes.data,
+    },
+  };
+
+  await saveExperimentData(expData);
+  return expData;
+}
+async function performMulti(variables: ExpVarMatrix, trials: number) {
+  if (!variables?.prompt?.length) {
+    variables.prompt = prompts;
+  }
+  const varCombs = genValueCombinations(variables);
   logger.info(
-    `Running experiment ${name} with ${trials} trials on models [gpt35turbo, gpt4, gpt4turbo], datasets ${datasetIds} and prompts ${Object.keys(
-      prompts
-    )}.`
+    `Preparing to run experiment ${name}, ${trials} times on each variable combination:\n${varCombs
+      .map(vc => "\t" + JSON.stringify(getVarIds(vc)))
+      .join(",\n")}.`
   );
-
-  const res: TrialsResult[] = [];
-  for (const modelId in models) {
-    for (const promptId in prompts) {
-      for (const dsId in datasets) {
-        const trialsRes = await runTrialsModel(
-          trials,
-          models[modelId as keyof typeof models],
-          dsId,
-          promptId
-        );
-        res.push({
-          variables: {
-            modelId,
-            promptId,
-            dsId,
-          },
-          data: trialsRes,
-        });
-      }
-    }
+  const res = [] as ExperimentData[];
+  for (const vc of varCombs) {
+    res.push(await perform(vc, trials, Date.now()));
   }
-  logger.info(`Results: ${JSON.stringify(res)}`);
   return res;
 }
 
 async function validate(trialsRes: TrialsResult[]) {}
+
+export async function saveExperimentData(data: ExperimentData) {
+  const ts = data.meta.traceId;
+  const dsId = data.variables.dataset.id;
+  const expName = data.meta.name;
+  const modelId = data.variables.model.id;
+  const rootFolder = "./results";
+  const filename = `${rootFolder}/${ts}_${expName}_${dsId}_${modelId}.json`;
+  const json = JSON.stringify(data, null, 2);
+
+  logger.info(
+    `Saving experiment ${data.meta.name} with traceId ${
+      data.meta.traceId
+    } to ${filename}. It ran ${
+      data.results.raw.length
+    } times with variables ${JSON.stringify(getVarIds(data.variables))}.`
+  );
+
+  if (!oldFs.existsSync(rootFolder)) {
+    await fs.mkdir(rootFolder);
+  }
+
+  await fs.writeFile(filename, json);
+}
 
 const ComparePromptsExperiment = {
   name,
   description,
   prompts,
   schema: resultSchema,
-  runTrials,
+  performMulti,
   validate,
 };
 
--- src/scripts/comparePrompts.ts
+++ src/scripts/comparePrompts.ts
@@ -1,16 +1,23 @@
-import { comparePrompts } from "../lib/experiments";
+import { ExpVarMatrix, comparePrompts } from "../lib/experiments";
+import { gpt35turbo, gpt4, gpt4turbo } from "../lib/models";
 import logger from "../lib/logger";
+import ws353 from "punuy-datasets/datasets/ws353";
+import simlex999 from "punuy-datasets/datasets/simlex999";
 
 const trials = process.argv[2] ? parseInt(process.argv[2]) : 1;
 
-const comparePromptsMain = async () => {
+const comparePromptsMain = async (vars: ExpVarMatrix) => {
   logger.info("Starting");
-  const trialsRes = await comparePrompts.runTrials(trials);
-
-  await comparePrompts.validate(trialsRes);
+  const res = await comparePrompts.performMulti(vars, trials);
+  console.log(res);
 };
 
-comparePromptsMain().then(() => {
+const evm: ExpVarMatrix = {
+  dataset: [ws353, simlex999],
+  model: [gpt35turbo, gpt4, gpt4turbo],
+};
+
+comparePromptsMain(evm).then(() => {
   logger.info("Done");
   process.exit(0);
 });
