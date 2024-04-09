beee18f069d8777b808f89623b9e6f21928707e1
André Santos <andrefs@andrefs.com>
2024-04-09 23:19:17 +0100

    wip

--- src/lib/dataset-adapters/collection.ts
+++ src/lib/dataset-adapters/collection.ts
@@ -1,4 +1,4 @@
-import { DatasetProfile } from "../types";
+// import { DatasetProfile } from "punuy-datasets/types";
 
 export interface MultiDatasetScores {
   [w1: string]: {
@@ -8,59 +8,59 @@ export interface MultiDatasetScores {
   };
 }
 
-export interface DatasetScores {
-  [w1: string]: {
-    [w2: string]: number;
-  };
-}
-
-export async function loadDatasetScores(dsId: string) {
-  const d = (await import(`punuy-datasets/datasets/${dsId}`)) as DatasetProfile;
-  const res = {} as DatasetScores;
-  for (const part of d.partitions) {
-    for (const row of part.data) {
-      const w1 = row.term1.toLowerCase();
-      const w2 = row.term2.toLowerCase();
-      if ("value" in row && row.value !== undefined) {
-        res[w1] = res[w1] || {};
-        res[w1][w2] = row.value;
-        continue;
-      }
-      if ("values" in row && Array.isArray(row.values)) {
-        const vals = row.values.filter(v => typeof v === "number") as number[];
-        res[w1] = res[w1] || {};
-        res[w1][w2] = vals.reduce((a, b) => a + b, 0) / vals.length;
-      }
-    }
-  }
-  return res;
-}
-
-export async function loadAllDatasetScores() {
-  const ds = (await import("punuy-datasets")).default;
-  const res = {} as MultiDatasetScores;
-
-  for (const dataset in ds) {
-    const d = ds[dataset as keyof typeof ds];
-    for (const part of d.partitions) {
-      for (const row of part.data) {
-        const w1 = row.term1.toLowerCase();
-        const w2 = row.term2.toLowerCase();
-
-        res[w1] = res[w1] || {};
-        res[w1][w2] = res[w1][w2] || {};
-        if ("value" in row && row.value !== undefined) {
-          res[w1][w2][dataset] = row.value;
-          continue;
-        }
-        if ("values" in row && Array.isArray(row.values)) {
-          const vals = row.values.filter(
-            v => typeof v === "number"
-          ) as number[];
-          res[w1][w2][dataset] = vals.reduce((a, b) => a + b, 0) / vals.length;
-        }
-      }
-    }
-  }
-  return res;
-}
+// interface DatasetScores {
+//   [w1: string]: {
+//     [w2: string]: number;
+//   };
+// }
+//
+// async function loadDatasetScores(dsId: string) {
+//   const d = (await import(`punuy-datasets/datasets/${dsId}`)) as DatasetProfile;
+//   const res = {} as DatasetScores;
+//   for (const part of d.partitions) {
+//     for (const row of part.data) {
+//       const w1 = row.term1.toLowerCase();
+//       const w2 = row.term2.toLowerCase();
+//       if ("value" in row && row.value !== undefined) {
+//         res[w1] = res[w1] || {};
+//         res[w1][w2] = row.value;
+//         continue;
+//       }
+//       if ("values" in row && Array.isArray(row.values)) {
+//         const vals = row.values.filter(v => typeof v === "number") as number[];
+//         res[w1] = res[w1] || {};
+//         res[w1][w2] = vals.reduce((a, b) => a + b, 0) / vals.length;
+//       }
+//     }
+//   }
+//   return res;
+// }
+//
+// async function loadAllDatasetScores() {
+//   const ds = (await import("punuy-datasets")).default;
+//   const res = {} as MultiDatasetScores;
+//
+//   for (const dataset in ds) {
+//     const d = ds[dataset as keyof typeof ds];
+//     for (const part of d.partitions) {
+//       for (const row of part.data) {
+//         const w1 = row.term1.toLowerCase();
+//         const w2 = row.term2.toLowerCase();
+//
+//         res[w1] = res[w1] || {};
+//         res[w1][w2] = res[w1][w2] || {};
+//         if ("value" in row && row.value !== undefined) {
+//           res[w1][w2][dataset] = row.value;
+//           continue;
+//         }
+//         if ("values" in row && Array.isArray(row.values)) {
+//           const vals = row.values.filter(
+//             v => typeof v === "number"
+//           ) as number[];
+//           res[w1][w2][dataset] = vals.reduce((a, b) => a + b, 0) / vals.length;
+//         }
+//       }
+//     }
+//   }
+//   return res;
+// }
